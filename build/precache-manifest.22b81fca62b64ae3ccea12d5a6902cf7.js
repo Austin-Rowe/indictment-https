8e16a25be138b8b69a4b6325e554c333fb039e09
Austin Rowe <amrowe2018@gmail.com>
2019-11-21 00:59:57 -0600

    New build with google analytics and some styling updates.

---
+++ build/precache-manifest.22b81fca62b64ae3ccea12d5a6902cf7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "11a5c7be4831dcb8dff4",
+    "url": "/static/css/main.63267458.chunk.css"
+  },
+  {
+    "revision": "11a5c7be4831dcb8dff4",
+    "url": "/static/js/main.11a5c7be.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "95cae063fb959796d0c4",
+    "url": "/static/js/2.95cae063.chunk.js"
+  },
+  {
+    "revision": "b9110ee8cae3f809261321fad9818480",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.084d540eb636dc02b7d7eddd1c4b3f98.js"
+  "/precache-manifest.22b81fca62b64ae3ccea12d5a6902cf7.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
